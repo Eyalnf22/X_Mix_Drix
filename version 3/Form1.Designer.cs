08c6e646e9459b9e74a49bdd46e90f776c47632f
Eyalnf22 <eyalnaftaly84@gmail.com>
2022-06-28 15:12:11 +0300

    ver3

---
+++ version 3/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FormX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl = new System.Windows.Forms.Panel();
+            this.title = new System.Windows.Forms.Label();
+            this.myTurn = new System.Windows.Forms.Label();
+            this.afterPnl = new System.Windows.Forms.Panel();
+            this.newGame = new System.Windows.Forms.Button();
+            this.pnl.SuspendLayout();
+            this.afterPnl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl
+            // 
+            this.pnl.Controls.Add(this.afterPnl);
+            this.pnl.Location = new System.Drawing.Point(41, 84);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(378, 354);
+            this.pnl.TabIndex = 0;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(125, 21);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(164, 37);
+            this.title.TabIndex = 1;
+            this.title.Text = "X Mix Drix";
+            // 
+            // myTurn
+            // 
+            this.myTurn.AutoSize = true;
+            this.myTurn.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myTurn.ForeColor = System.Drawing.Color.Firebrick;
+            this.myTurn.Location = new System.Drawing.Point(12, 54);
+            this.myTurn.Name = "myTurn";
+            this.myTurn.Size = new System.Drawing.Size(80, 27);
+            this.myTurn.TabIndex = 2;
+            this.myTurn.Text = "turn: X";
+            // 
+            // afterPnl
+            // 
+            this.afterPnl.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.afterPnl.Controls.Add(this.newGame);
+            this.afterPnl.Location = new System.Drawing.Point(74, 112);
+            this.afterPnl.Name = "afterPnl";
+            this.afterPnl.Size = new System.Drawing.Size(200, 100);
+            this.afterPnl.TabIndex = 3;
+            this.afterPnl.Visible = false;
+            // 
+            // newGame
+            // 
+            this.newGame.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newGame.Location = new System.Drawing.Point(30, 30);
+            this.newGame.Name = "newGame";
+            this.newGame.Size = new System.Drawing.Size(144, 42);
+            this.newGame.TabIndex = 0;
+            this.newGame.Text = "New Game";
+            this.newGame.UseVisualStyleBackColor = true;
+            this.newGame.Click += new System.EventHandler(this.newGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 450);
+            this.Controls.Add(this.myTurn);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.pnl);
+            this.Name = "Form1";
+            this.Text = "X Mix Drix";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnl.ResumeLayout(false);
+            this.afterPnl.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label myTurn;
+        private System.Windows.Forms.Panel afterPnl;
+        private System.Windows.Forms.Button newGame;
+    }
+}
+
---
+++ version 3/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormX
+{
+    public partial class Form1 : Form
+    {
+        int row = 3;
+        int size = 60;
+        int space = 100;
+        bool turn = true;
+        Button[,] btns;
+        //if turn is true therfore X is palying, false means O plays.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateBoard();
+        }
+
+        //sets the buttons on the board
+        private void CreateBoard()
+        {
+            btns = new Button[row, row];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    Button b = new Button();
+                    b.Size = new Size(size, size);
+                    b.Location = new Point(i * space, j * space);
+                    b.Click += choose;
+                    b.Font = new Font("Arial", 24, FontStyle.Bold);
+                    btns[i, j] = b;
+
+                    pnl.Controls.Add(b);
+                }
+            }
+        }
+
+        //mark the player's chosie and switch turns.
+        private void choose(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            if (b.Text == "")
+            {
+                if (turn)
+                {
+                    myTurn.Text = "Turn: O";
+                    b.Text = "X";
+                }
+                else
+                {
+                    myTurn.Text = "Turn: X";
+                    b.Text = "O";
+                }
+
+                if (checkWin())
+                {
+                    string txt = "";
+                    if (turn)
+                        txt = "X Wins!";
+                    else
+                        txt = "O Wins!";
+                    title.Text = txt;
+
+                    DisableGame();
+                }
+                turn = !turn;
+
+            }
+        }
+
+        //disable the buttons.
+        private void DisableGame()
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    btns[i, j].Enabled = false;
+                }
+            }
+            afterPnl.Visible = true;
+        }
+
+        //cheks for a winner for a row, colom or slope.
+        private bool checkWin()
+        {
+            for (int i = 0; i < row; i++)
+            {
+                if (btns[i, 0].Text == btns[i, 1].Text && btns[i, 2].Text == btns[i, 1].Text && btns[i, 1].Text != "")
+                    return true;
+                if (btns[0, i].Text == btns[1, i].Text && btns[2, i].Text == btns[1, i].Text && btns[1, i].Text != "")
+                    return true;
+            }
+            if (btns[0, 2].Text == btns[1, 1].Text && btns[1, 1].Text == btns[2, 0].Text && btns[2, 0].Text != "")
+                return true;
+            else if (btns[0, 0].Text == btns[1, 1].Text && btns[1, 1].Text == btns[2, 2].Text && btns[1, 1].Text != "")
+                return true;
+
+            return false;
+        }
+
+        //restart the game
+        private void newGame_Click(object sender, EventArgs e)
+        {
+            turn = true;
+           
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    btns[i, j].Enabled = true;
+                    btns[i, j].Text = "";
+                }
+            }
+            afterPnl.Visible = false;
+            myTurn.Text = "Turn: X";
+
+
+        }
+    }
+}
+
